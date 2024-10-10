67f4e20dd59128dcb8527880524e46e138a97ffb
qurat-iinv <quratulain@iinv.tech>
2024-10-10 11:12:53 +0500

    layout changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,73 +1,78 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
-import { Colors } from "@/constants/Colors";
+import { COLORS, Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { FontAwesome } from "@expo/vector-icons";
-import { Image, View } from "react-native";
 import { HomeFocusedTab, HomeOutlinedTab } from "@/assets";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
-        tabBarStyle: {
-          height: 70, // Adjust the height as needed
-          paddingBottom: 10, // Optional: Adjust the padding for icons/texts inside the tab bar
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, focused }) =>
-            focused ? <HomeFocusedTab /> : <HomeOutlinedTab />,
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: "Search",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "code-slash" : "code-slash-outline"}
-              color={color}
-            />
-          ),
-        }}
-      />
+    <>
+      <StatusBar backgroundColor={COLORS.primary} />
+      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+        <Tabs
+          screenOptions={{
+            tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+            headerShown: false,
+            tabBarStyle: {
+              height: 70,
+              paddingBottom: 10,
+            },
+          }}
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Home",
+              tabBarIcon: ({ color, focused }) =>
+                focused ? <HomeFocusedTab /> : <HomeOutlinedTab />,
+            }}
+          />
+          <Tabs.Screen
+            name="search"
+            options={{
+              title: "Search",
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon
+                  name={focused ? "code-slash" : "code-slash-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
 
-      <Tabs.Screen
-        name="wishlist"
-        options={{
-          title: "wishlist",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "code-slash" : "code-slash-outline"}
-              color={color}
-            />
-          ),
-        }}
-      />
+          <Tabs.Screen
+            name="wishlist"
+            options={{
+              title: "wishlist",
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon
+                  name={focused ? "code-slash" : "code-slash-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
 
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "code-slash" : "code-slash-outline"}
-              color={color}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+          <Tabs.Screen
+            name="profile"
+            options={{
+              title: "Profile",
+              tabBarIcon: ({ color, focused }) => (
+                <TabBarIcon
+                  name={focused ? "code-slash" : "code-slash-outline"}
+                  color={color}
+                />
+              ),
+            }}
+          />
+        </Tabs>
+      </SafeAreaView>
+    </>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { ActivityIndicator } from "react-native-paper";
-import { useFonts } from "expo-font";
+import { ScrollView } from "react-native";
 
 import {
   Card,
@@ -9,68 +6,47 @@ import {
   Container,
   Header,
   HorizontalFlatlist,
-  ScrollableCards,
   SearchField,
   Typography,
 } from "@/components";
-import { ScrollView, TouchableOpacity } from "react-native";
-import { useState } from "react";
 
 export default function HomeScreen() {
-  let [fontsLoaded] = useFonts({
-    LibreBodoni: require("./../../assets/fonts/static/LibreBodoni-Regular.ttf"),
-    LibreBodoniBold: require("./../../assets/fonts/static/LibreBodoni-Bold.ttf"),
-    LibreBodoniMedium: require("./../../assets/fonts/static/LibreBodoni-Medium.ttf"),
-    Lato: require("./../../assets/fonts/Lato-Regular.ttf"),
-    LatoBold: require("./../../assets/fonts/Lato-Bold.ttf"),
-    LatoLight: require("./../../assets/fonts/Lato-Light.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return <ActivityIndicator size="large" color="#0000ff" />;
-  }
-
   return (
-    <>
-      <StatusBar backgroundColor={"#D3AFC9"} />
-      <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+    <ScrollView>
+      <Container>
         <Header />
-        <ScrollView>
-          <Container>
-            <SearchField />
-            <ScrollableCards />
-            <Typography fontType="secondary" mT={24} size={18} bold>
-              Catagories
-            </Typography>
+        <SearchField />
+        {/* <ScrollableCards /> */}
+        <Typography fontType="secondary" mT={24} size={18} bold>
+          Catagories
+        </Typography>
 
-            <HorizontalFlatlist
-              data={[1, 2, 3, 4, 5, 6]}
-              renderItem={({ item }) => <Catagories mT={30} />}
-              contentContainerStyle={{ gap: 20 }}
-            />
+        <HorizontalFlatlist
+          data={[1, 2, 3, 4, 5, 6]}
+          renderItem={({ item }) => <Catagories mT={30} />}
+          contentContainerStyle={{ gap: 20 }}
+        />
 
-            <Typography fontType="secondary" mT={24} size={18}>
-              Most Popular 🔥
-            </Typography>
+        <Typography fontType="secondary" mT={24} size={18}>
+          Most Popular 🔥
+        </Typography>
 
-            <HorizontalFlatlist
-              data={[1, 2, 3]}
-              renderItem={({ item }) => <Card mT={30} />}
-              contentContainerStyle={{ gap: 20 }}
-            />
+        <HorizontalFlatlist
+          data={[1, 2, 3]}
+          renderItem={({ item }) => <Card mT={30} />}
+          contentContainerStyle={{ gap: 20 }}
+        />
 
-            <Typography fontType="secondary" mT={24} size={18}>
-              Upcoming Events
-            </Typography>
+        <Typography fontType="secondary" mT={24} size={18}>
+          Upcoming Events
+        </Typography>
 
-            <HorizontalFlatlist
-              data={[1, 2, 3]}
-              renderItem={({ item }) => <Card mT={30} />}
-              contentContainerStyle={{ gap: 20 }}
-            />
-          </Container>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        <HorizontalFlatlist
+          data={[1, 2, 3]}
+          renderItem={({ item }) => <Card mT={30} />}
+          contentContainerStyle={{ gap: 20 }}
+        />
+      </Container>
+    </ScrollView>
   );
 }
