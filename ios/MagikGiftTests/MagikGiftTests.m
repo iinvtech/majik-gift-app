3f362da1d1d290bfada846fdc2bb1f29ea766fb8
Muhammad Shahrukh <muhammad-shahrukh@iinv.tech>
2024-10-15 16:34:33 +0500

    ios setup and configuration and set ios app icon

--- ios/MagikGiftTests/MagikGiftTests.m
+++ ios/MagikGiftTests/MagikGiftTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNBoilerPlateTests : XCTestCase
+@interface MagikGiftTests : XCTestCase
 
 @end
 
-@implementation RNBoilerPlateTests
+@implementation MagikGiftTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -61,6 +61,13 @@ const styles = StyleSheet.create({
   main: {
     height: sizer.moderateScale(63),
     elevation: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
     backgroundColor: '#fff',
     paddingHorizontal: paddingHorizontal,
     marginHorizontal: -paddingHorizontal,
